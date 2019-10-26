d4794f4e4ec76a5e2230d212813884218cba2855
Justas Grekavicius <justas.grekavicius@gmail.com>
2019-10-26 13:14:08 +0300

    Styled index page. Routing to individual items

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
 require("dotenv").config();
 
-const express = require("express");
+const express = require("express"),
+  fetch = require("node-fetch");
 
 let app = express();
 
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
-app.get("/", function(req, res) {
-  res.render("index");
+const url = "http://bgg-json.azurewebsites.net/hot";
+
+async function getList() {
+  let response = await fetch(url);
+  let data = await response.json();
+  return data;
+}
+
+const gamesList = getList();
+
+app.get("/", async function(req, res) {
+  res.render("games/index", { games: await gamesList });
+});
+
+app.get("/games/:id", async function(req, res) {
+  let foundGame = (await gamesList).find(function(game) {
+    return game.gameId === Number(req.params.id);
+  });
+  console.log(typeof req.params.id);
+  res.render("games/show", { game: foundGame });
+});
+
+app.get("*", function(req, res) {
+  res.send("This page does not exist!");
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
